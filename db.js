2a60723b7bb4fba0592994d2bcbc8e6ea01a8da7
sreedhar-eg1 <sreedhareg1997@gmail.com>
2023-06-14 11:40:32 +0530

    Final commit

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config()
 
-<<<<<<< HEAD
-const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.upccyc9.mongodb.net/EMPLOYEES_DATA`)
-=======
->>>>>>> f047093f95868949e21dfcf508a94eaabbb42970
+const client = new MongoClient(process.env.CONNECTION_STRING)
+
 const PORT = process.env.PORT || 3000
 
 const start = async () => {
